675b14ac2552a014e7bb90200f825fe295a199a1
applebot-ai <68159225+applebot-ai@users.noreply.github.com>
2020-07-12 08:25:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,14 +1,19 @@
-# 誰かがメッセージをポストしたとき
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
 @client.event
 async def on_message(message):
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
 
-    # ボットのメッセージは無視
-    # 問答無用で無限ループ抑えるのがお作法らしい
-    if message.author.bot:
-        return
-
-    # /fizz と入力されたら、buzz とこたえる
-    if message.content == '/fizz':
-        await message.channel.send('buzz')
-
-    await message.channel.send(message.author.name + 'が語るぞ')
+client.run("token")
