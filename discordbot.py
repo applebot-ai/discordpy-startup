4cc8dc2bbb7ea6d6669f83160dfd57432242116f
applebot-ai <68159225+applebot-ai@users.noreply.github.com>
2020-07-12 18:39:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,10 @@
 import discord
 client = discord.Client()
 
+from discord.ext import commands
+import os
+import tracebak
 
+bot = commands.Bot(command_prefix="/")
+token = os.environ['DISCORD_BOT_TOKEN']
 
