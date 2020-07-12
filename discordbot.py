dc18c3a3d8e366700d8ee829e2b32d7938ce7ba4
applebot-ai <68159225+applebot-ai@users.noreply.github.com>
2020-07-12 20:18:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,22 +11,25 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('ponpoko')
 
 bot.run(token)
 
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
 
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
+
+bot = commands.Bot(command_prefix="/")
+token = os.environ['DISCORD_BOT_TOKEN']
+
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+@bot.command()
+async def builds(ctx):
+    await ctx.send('https://tt2-compendium.com/en/builds/')
+
+bot.run(token)
