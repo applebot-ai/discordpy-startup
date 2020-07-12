d3ec919f946f4b4cb3050d84f460694ab3bd0025
applebot-ai <68159225+applebot-ai@users.noreply.github.com>
2020-07-12 20:12:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -21,6 +21,9 @@ bot.run(token)
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
     # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == '/neko':
         await message.channel.send('にゃーん')
