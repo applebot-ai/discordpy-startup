1f602edcadd64e44a7e9819fbd2409e7c3560be9
applebot-ai <68159225+applebot-ai@users.noreply.github.com>
2020-07-13 07:01:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -38,7 +38,12 @@ async def petb(ctx):
 @bot.command()
 async def raid(ctx):
     await ctx.send('https://docs.google.com/spreadsheets/d/166QB9bVQE37K8KdnITG_cu-_cvi8iE78EFIWemdJ6X0/edit#gid=736827398')     
-    
+
+@bot.command()
+async def card(ctx):
+    await ctx.send('https://docs.google.com/spreadsheets/d/18vZdCWrcXAQKEraoSDWVFnT_jwk84iXEs6uAAPQ9OM0/edit')     
+   
+
 bot.run(token)
 
 
