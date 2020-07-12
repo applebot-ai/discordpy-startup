6516a6ba015ab0bf41cc874c2737762efc77881d
applebot-ai <68159225+applebot-ai@users.noreply.github.com>
2020-07-12 20:09:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,8 +18,12 @@ async def ping(ctx):
 
 bot.run(token)
 
-@bot.command()
-async def builds(ctx):
-    await ctx.send('https://tt2-compendium.com/en/builds')
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
-bot.run(token)
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
