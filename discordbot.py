764e1fe013dd26ddd9275f9ea12eed92a1328b73
applebot-ai <68159225+applebot-ai@users.noreply.github.com>
2020-07-12 23:05:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,26 +15,13 @@ async def on_command_error(ctx, error):
 async def peke(ctx):
     await ctx.send('ponpon')
 
-bot.run(token)
-
-
-from discord.ext import commands
-import os
-import traceback
-
-bot = commands.Bot(command_prefix="/")
-token = os.environ['DISCORD_BOT_TOKEN']
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
+    
 bot.run(token)
+
+
+
 
 
